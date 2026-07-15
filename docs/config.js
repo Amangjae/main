ad07e0347ab2c602b0f70a445350fa736e1e1b95
김재명 | 상품지원부 | 업무혁신팀 | 팀장 <jaemy82@modetour.com>
2026-07-15 17:19:52 +0900

    Connect lunch selection API

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,3 @@
 window.LUNCH_DATA_URL = "./data/site-data.json";
-window.LUNCH_SHEET_URL = "";
-window.LUNCH_ACTION_API_URL = "";
+window.LUNCH_SHEET_URL = "https://docs.google.com/spreadsheets/d/1ZdSoj2x0H2L-KWMzU7yo5Pp9WfnyBbxxnoHF3Y7bVyw/edit?gid=0#gid=0";
+window.LUNCH_ACTION_API_URL = "https://script.google.com/macros/s/AKfycbzHlG10wZoCgsKpD8p1HYQtvsyq08ZGaqg0ZkYUksUVEyB22X-Gz9wtDJfC_-I_XfKWnw/exec";
